cda399e3cc96e7c85f2d96a5679090d47e4300db
pk <pk@mp-electronics.pl>
2016-09-07 23:07:57 +0200

    stage 4 changes

--- XAIOT.MobileApps/XAIOT.Workshop/MPED.StacjaIT.XAIOT/MPED.StacjaIT.XAIOT.WorkshopApp/MPED.StacjaIT.XAIOT.WorkshopApp.WinPhone/ControlCircuitPage.xaml.cs
+++ XAIOT.MobileApps/XAIOT.Workshop/MPED.StacjaIT.XAIOT/MPED.StacjaIT.XAIOT.WorkshopApp/MPED.StacjaIT.XAIOT.WorkshopApp.WinPhone/ControlCircuitPage.xaml.cs
@@ -26,14 +26,10 @@ namespace MPED.StacjaIT.XAIOT.WorkshopApp.WinPhone
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class ControlCircuitPage : Page
-    {
-        private BuildingWebService _buildingWebService = null;
-        private SystemTasksWebService _systemTasksWebService = null;
+    {        
         public ControlCircuitPage()
         {
-            this.InitializeComponent();
-            _buildingWebService = new BuildingWebService();
-            _systemTasksWebService = new SystemTasksWebService();
+            this.InitializeComponent();            
         }
 
         /// <summary>
@@ -43,51 +39,13 @@ namespace MPED.StacjaIT.XAIOT.WorkshopApp.WinPhone
         /// This parameter is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            DataContext = e.Parameter;
+            
         }
 
         private void BackButton_Tapped(object sender, TappedRoutedEventArgs e)
         {
             if (Frame.CanGoBack)
                 Frame.GoBack();
-        }
-
-        private async void TurnOnButton_Click(object sender, RoutedEventArgs e)
-        {
-            ControlCircuitViewModel controlCircuitViewModel = DataContext as ControlCircuitViewModel;
-            if (controlCircuitViewModel != null && controlCircuitViewModel.I2CSlaveAddress == "0x40")
-            {
-                await _systemTasksWebService.PostSystemTask(App.Token.access_token, new Models.Tasks.SystemTask() { CreatedOn = DateTime.Now, Id = Guid.NewGuid(), InputData = JsonConvert.SerializeObject(new DeviceBasicInputData() { ControlCircuit = "0x40", DevicePin = 24, ZoneId = new Guid("835dfb35-479c-49d6-ad06-fd8f083b7f99") }), Name = "TurnOnDevice", Status = 1, Type = 1, UpdatedOn = DateTime.Now });
-            }
-            else
-            {                
-                MessageDialog msgbox = new MessageDialog("Sterownik nie posiada tego typu urządzenia.");
-                await msgbox.ShowAsync();
-            }
-        }
-
-        private async void TurnOffButton_Click(object sender, RoutedEventArgs e)
-        {
-            ControlCircuitViewModel controlCircuitViewModel = DataContext as ControlCircuitViewModel;
-            if (controlCircuitViewModel != null && controlCircuitViewModel.I2CSlaveAddress == "0x40")
-            {
-                await _systemTasksWebService.PostSystemTask(App.Token.access_token, new Models.Tasks.SystemTask() { CreatedOn = DateTime.Now, Id = Guid.NewGuid(), InputData = JsonConvert.SerializeObject(new DeviceBasicInputData() { ControlCircuit = "0x40", DevicePin = 24, ZoneId = new Guid("835dfb35-479c-49d6-ad06-fd8f083b7f99") }), Name = "TurnOffDevice", Status = 1, Type = 2, UpdatedOn = DateTime.Now });
-            }
-            else
-            {
-                MessageDialog msgbox = new MessageDialog("Sterownik nie posiada tego typu urządzenia.");
-                await msgbox.ShowAsync();
-            }
-        }
-
-        private async void RefreshButton_Click(object sender, RoutedEventArgs e)
-        {
-            ControlCircuitViewModel controlCircuitViewModel = DataContext as ControlCircuitViewModel;
-            if (controlCircuitViewModel != null)
-            {
-                App.Building = await _buildingWebService.FindBuildingFullData(App.Token.access_token, new Guid("1888561c-950b-41f3-9963-ddbeceb4da22"));
-                DataContext = App.Building.Zones.SelectMany(q => q.ControlCircuits).FirstOrDefault(q => q.Id == controlCircuitViewModel.Id);
-            }
-        }
+        }        
     }
 }
